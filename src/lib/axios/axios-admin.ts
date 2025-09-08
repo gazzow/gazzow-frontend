e60c7990e36f157914550c6df3e08ae5fc513162
Abbas firdous <abbasfathima40@gmail.com>
2025-09-08 19:13:48 +0530

    chore: admin axios base url update with prefix /admin

--- src/lib/axios/axios-admin.ts
+++ src/lib/axios/axios-admin.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosAdmin = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL + "/admin",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
