4e9d873ffa6ad4d0e81feadf8102d111f82b3c5c
Abbas firdous <abbasfathima40@gmail.com>
2025-09-09 21:14:57 +0530

    chore: add dashboard ui subheader

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,7 +1,11 @@
 function Dashboard() {
   return (
-    <div className="text-white">
-      <h1 >Dashboard</h1>
+    <div className="p-8">
+      {/* Header */}
+      <div className="flex p-4 items-center justify-between mb-6 border border-border-primary rounded-lg">
+        <h1 className="text-2xl font-bold text-white">Admin Dashboard</h1>
+
+      </div>
     </div>
   );
 }
