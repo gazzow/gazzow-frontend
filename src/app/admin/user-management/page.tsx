1d932b4b5e58469009667eb45445cab5fb913664
Abbas firdous <abbasfathima40@gmail.com>
2025-09-30 22:45:25 +0530

    refactor: confirm modal button style update

--- src/app/admin/user-management/page.tsx
+++ src/app/admin/user-management/page.tsx
@@ -155,18 +155,18 @@ export default function UserManagement() {
                 ? "Are you sure you want to block this user?"
                 : "Are you sure you want to activate this user?"}
             </p>
-            <div className="flex justify-end gap-2 mt-4">
+            <div className="flex justify-end gap-4 mt-4">
               <button
-                className="bg-green-400 py-1 px-4 rounded text-white"
-                onClick={confirmToggleStatus}
-              >
-                Yes
-              </button>
-              <button
-                className="bg-red-400 py-1 px-4 rounded "
+                className="bg-red-400 text-white py-1 px-4 rounded cursor-pointer"
                 onClick={() => setConfirmModalOpen(false)}
               >
                 No
+              </button>
+              <button
+                className="bg-green-400 py-1 px-4 rounded text-gray cursor-pointer"
+                onClick={confirmToggleStatus}
+              >
+                Yes
               </button>
             </div>
           </div>
