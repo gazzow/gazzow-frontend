95c0ae526b976d6a85eb37464f15d1b7b5f2a21e
Abbas firdous <abbasfathima40@gmail.com>
2025-11-18 11:00:46 +0530

    feat: create re-usable confirm modal component

--- src/components/features/ConfirmModal.tsx
+++ src/components/features/ConfirmModal.tsx
@@ -1,24 +1,37 @@
-export default function ConfirmModal() {
+interface ConfirmModal {
+  title: string;
+  message: string;
+  confirmLabel: string;
+  cancelLabel: string;
+  onCancel: () => void;
+  onConfirm?: () => void;
+}
+
+export default function ConfirmModal({
+  title,
+  message,
+  confirmLabel,
+  cancelLabel,
+  onCancel,
+  onConfirm,
+}: ConfirmModal) {
   return (
     <div className="fixed inset-0 bg-primary/60  flex justify-center items-center">
-      <div className="bg-white dark:bg-secondary p-4 rounded shadow flex flex-col">
-        <p className="font-bold text-black dark:text-white">
-          {"condition" === "condition"
-            ? "Are you sure you want to block this user?"
-            : "Are you sure you want to activate this user?"}
-        </p>
-        <div className="flex justify-end gap-4 mt-4">
+      <div className="bg-white dark:bg-secondary p-4 rounded justify-center items-center shadow flex flex-col gap-3">
+        <h2 className="text-xl font-medium text-black dark:text-white">{title}</h2>
+        <p className="font-semibold text-black dark:text-white">{message}</p>
+        <div className="flex justify-end gap-4">
           <button
-            className="bg-red-400 text-white py-1 px-4 rounded cursor-pointer"
-            // onClick={() => setConfirmModalOpen(false)}
+            className="bg-red-400 text-white py-1 px-4 rounded cursor-pointer hover:bg-red-500"
+            onClick={onCancel}
           >
-            No
+            {cancelLabel}
           </button>
           <button
-            className="bg-green-400 py-1 px-4 rounded text-gray cursor-pointer"
-            // onClick={confirmToggleStatus}
+            className="bg-green-400 py-1 px-4 rounded text-gray-950 cursor-pointer hover:bg-green-500"
+            onClick={onConfirm}
           >
-            Yes
+            {confirmLabel}
           </button>
         </div>
       </div>
