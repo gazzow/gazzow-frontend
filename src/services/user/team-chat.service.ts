e781de933811347bc975b679b09930e615acebff
Abbas firdous <abbasfathima40@gmail.com>
2026-01-04 15:39:39 +0530

    refactor: replace hardcoded api endpoint with constant object

--- src/services/user/team-chat.service.ts
+++ src/services/user/team-chat.service.ts
@@ -1,8 +1,9 @@
+import { TEAM_CHAT_API } from "@/constants/apis/team-chat";
 import api from "@/lib/axios/api";
 
 export const teamChatService = {
   async listMessages(projectId: string) {
-    const res = await api.get(`/team-chat/${projectId}`);
+    const res = await api.get(TEAM_CHAT_API.LIST_TEAM_CHAT(projectId));
     console.log("List team chats response: ", res);
     return res.data;
   },
