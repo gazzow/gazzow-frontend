a66ac1fe09966055761f0ee8ed0f2efd167d4c6e
Abbas firdous <abbasfathima40@gmail.com>
2026-02-12 10:13:16 +0530

    chore: remove unused imports

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { USER_ROUTES } from "@/constants/routes/user-routes";
-import { useTheme } from "@/hook/useTheme";
-import { Moon, Sun } from "lucide-react";
 import Link from "next/link";
 import UserProfileMenu from "../ui/UserProfileMenu";
 import { NotificationBellIcon } from "../ui/NotificationBellIcon";
 import { ThemeToggle } from "../ui/ThemeToggle";
 
 export default function Navbar() {
-  const { theme, toggleTheme } = useTheme();
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-white dark:bg-primary backdrop-blur-md border-b border-border-primary/70 transition ease-in-out">
