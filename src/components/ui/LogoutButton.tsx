f4fa79a10fa9a0f3db7b7b938f174aa99a048bda
Abbas firdous <abbasfathima40@gmail.com>
2026-02-12 17:16:25 +0530

    ui: update logout button style

--- src/components/ui/LogoutButton.tsx
+++ src/components/ui/LogoutButton.tsx
@@ -2,19 +2,13 @@
 
 import { useLogout } from "@/hook/useLogout";
 import { LogOut } from "lucide-react";
-import { usePathname } from "next/navigation";
 
 export function LogoutButton() {
   const { handleLogout } = useLogout();
-  const pathname = usePathname();
-  const isRootPage = pathname === "/";
-
   return (
     <button
       onClick={handleLogout}
-      className={`w-full flex items-center gap-3 px-3 py-2 rounded-md transition cursor-pointer
-    ${isRootPage ? "bg-red-500/30" : "hover:bg-gray-200 dark:hover:bg-secondary"}
-    text-black dark:text-white`}
+      className={`w-full flex items-center gap-3 px-3 py-2 rounded-md cursor-pointer hover:text-red-600 hover:bg-red-100 dark:hover:bg-secondary text-black dark:text-white transition transform duration-200`}
     >
       <LogOut className="w-4 h-4" />
       <span className="hidden md:flex">Log out</span>
--- src/components/ui/UserProfileMenu.tsx
+++ src/components/ui/UserProfileMenu.tsx
@@ -112,7 +112,7 @@ export default function UserProfileMenu() {
             w-full flex items-center gap-2 px-3 py-2 rounded-md
             text-slate-700 dark:text-slate-200
             hover:bg-slate-100 dark:hover:bg-slate-800
-            transition
+            transition cursor-pointer
           "
             >
               <User size={18} />
@@ -126,7 +126,7 @@ export default function UserProfileMenu() {
             w-full flex items-center gap-2 px-3 py-2 rounded-md
             text-red-600 dark:text-red-400
             hover:bg-red-50 dark:hover:bg-red-900/30
-            transition
+            transition cursor-pointer
           "
             >
               <LogOut size={18} />
